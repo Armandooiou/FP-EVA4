b36776ada01648979baba3353fc28542052dd20f
Armandooiou <L24550279@chihuahua2.tecnm.mx>
2024-08-31 17:42:18 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL/src/main/java/com/mycompany/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_automovil;
+
+/**
+ *
+ * @author aaro1
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //DECLARACION DE LAS VARIABLES 
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+                
+        //ASIGNAR VALOR 
+        marca ="Ford";
+        modelo = "Mustang";
+        year = 2022;
+        precio = 999000.1;
+        
+        System.out.println(marca); //Imprimir el contenido de las variables 
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+    
+    
+    
+    }
+}
